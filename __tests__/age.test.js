57f9e1beaa1ca70357a415ac4aa781ec09f24561
Albert Lee <leesga8@gmail.com>
2021-11-12 13:12:35 -0800

    refactor beforeeach(). BLUE

--- __tests__/age.test.js
+++ __tests__/age.test.js
@@ -1,13 +1,15 @@
 import Person from './../src/js/age'
 
 describe('Person', () => {
+  let person;
 
+  beforeEach(() => {
+    person = new Person(20)
+  })
   test('should create an person object with 1 age parameter', () => {
-    const person = new Person(20);
     expect(person.age).toEqual(20);
   })
   test('should return age divided by .24 to equal mercury years', () =>{
-    const person = new Person(20)
     expect(person.mercuryAge()).toEqual(83);
   })
 });
