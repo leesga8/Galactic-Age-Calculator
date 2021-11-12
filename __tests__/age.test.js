b00a4427ee7af1fd064f861cfc951964f14e9857
Albert Lee <leesga8@gmail.com>
2021-11-12 11:53:41 -0800

    fix typos. pass first test GREEN

--- __tests__/age.test.js
+++ __tests__/age.test.js
@@ -1,9 +1,9 @@
-import Age from '../src/js/age.js';
+import Person from './../src/js/age'
 
-describe('Age', () => {
+describe('Person', () => {
 
-  test('should create an age object with one parameter', () => {
-    const newAge = new Age(20);
-    expect(newAge.age).toEqual(20);
+  test('should create an person object with 1 age parameter', () => {
+    const person = new Person(20);
+    expect(person.age).toEqual(20);
   })
 });
